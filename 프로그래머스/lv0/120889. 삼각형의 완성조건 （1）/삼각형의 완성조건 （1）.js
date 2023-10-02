a2d6922a935837745a508b2567515f337afacbf9
Seoyeong Min <136035516+Seo0min@users.noreply.github.com>
2023-10-02 11:03:48 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,10 @@
+function solution(sides) {
+    sides.sort(function(a, b) {
+        return a - b;
+    })
+    if(sides[2] < (sides[0]+sides[1])){
+        return 1
+    } else {
+        return 2
+    }
+}
