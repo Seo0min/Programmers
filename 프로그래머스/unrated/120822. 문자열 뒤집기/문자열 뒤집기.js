901e480dd6231ad7c1f67c2244fa7707bbe13c47
Seoyeong Min <136035516+Seo0min@users.noreply.github.com>
2023-12-02 11:13:08 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    let answer = '';
+    let splitString = my_string.split("");
+    let reverseString = splitString.reverse();
+    answer = reverseString.join("");
+    return answer;
+}
